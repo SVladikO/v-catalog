4eba381f7157b8a0a36d66719efc5776fb384d86
svladiko <svladiko@mail.ru>
2021-06-19 09:56:37 +0300

    fix

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,8 +3,4 @@ import translations from "../common.dictionary";
 export function resolveTranslation(key, currentLanguage) {
     return translations[key.substring(1)][currentLanguage];
 }
-export function resolveString(key) {
-    let currentLanguage = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).config).currentLanguage;
-    return translations[key.substring(1)][currentLanguage];
-}
 
