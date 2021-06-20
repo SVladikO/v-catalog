cfe27383a7854e9eaf04fbffd3f26bfaf4af9a19
svladiko <svladiko@mail.ru>
2021-06-20 22:37:41 +0300

    fix

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -9,35 +9,42 @@ const profileLinks = [
     {
         href: 'https://github.com/SVladikO',
         imageComponent: <GithubIcon/>,
-        alt: 'github'
     },
     {
         href: 'https://www.linkedin.com/in/vlad-serhiychuk-b753b7188/',
         imageComponent: <LinkedinIcon/>,
-        alt: 'linkedin'
     }
 ];
+
 
 function Footer() {
     return (
         <div className="footer">
-            <div className="profile_container">
-                <span className="profile_title">
-                    <Text translationKey="$FOOTER.PROFILE.TITLE"/>
-                </span>
-                <div className="profile_links">
-                    {
-                        profileLinks.map(link =>
-                            <a href={link.href} alt={link.alt} target="_blank" alt={link.alt}>
-                                {link.imageComponent}
-                            </a>
-                        )
-                    }
-                </div>
+            {
+                getProfilePart()
+            }
+        </div>
+    )
+}
 
+function getProfilePart() {
+    return (
+        <div className="profile_container">
+            <div className="profile_title">
+                <Text translationKey="$FOOTER.PROFILE.TITLE"/>
+            </div>
+            <div className="profile_links">
+                {
+                    profileLinks.map(link =>
+                        <a href={link.href} target="_blank">
+                            {link.imageComponent}
+                        </a>
+                    )
+                }
             </div>
         </div>
     )
 }
+
 
 export default Footer;
