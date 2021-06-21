eadbc800f3f49f0bd457074748551bb5f9ea9360
Vlad Serhiychuk <SVladikO@mail.ru>
2021-06-21 19:09:10 +0300

    0026 Added companies where I worked in introduction section. (#45)
    * 0026 Added companies where I worked in introduction section.

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Introduction from "../../components/intro/introduction.component";
+import Introduction from "../../components/introduction/introduction.component";
 
 function HomePage() {
     return (
