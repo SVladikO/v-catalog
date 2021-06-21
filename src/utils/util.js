202344774f31f5ec76dba6369562f9a7c949df62
Vlad Serhiychuk <SVladikO@mail.ru>
2021-06-21 12:56:12 +0300

    0022 Fixed empty translations. (#36)

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,6 +1,6 @@
 import translations from "../common.dictionary";
 
-export function resolveTranslation(key, currentLanguage) {
+export function resolveTranslation(key, currentLanguage = "en") {
     return translations[key.substring(1)][currentLanguage];
 }
 
