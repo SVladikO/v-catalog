5016d2c7beb42adbbd51fe52a801a5851eee6d84
svladiko <vlad.serhiychuk@gmail.com>
2022-08-19 17:27:20 +0300

    VC-130 Added localStorage for income calculator, deleted checkboxes

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,14 +2,10 @@ import {useState, useEffect} from 'react';
 
 function getSavedValue(key, initialValue) {
     const savedValue = JSON.parse(localStorage.getItem(key))
-    console.log('getSavedValue', key, {savedValue}, {initialValue}, 'value: ', localStorage.getItem(key));
-    if (savedValue) {
-        return savedValue
-    }
 
-    if (initialValue instanceof Function) {
-        return initialValue();
-    }
+    if (savedValue) return savedValue;
+
+    if (initialValue instanceof Function) return initialValue();
 
     return initialValue;
 }
@@ -20,7 +16,6 @@ export default function useLocalStorage(key, initialValue) {
     );
 
     useEffect(() => {
-        console.log('key: ', value)
         localStorage.setItem(key, JSON.stringify(value));
     }, [value]);
 
