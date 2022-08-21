bd395fa4433add91e4c5e03b06d4675a15036515
svladiko <vlad.serhiychuk@gmail.com>
2022-08-21 19:47:43 +0300

    CV-151 Added hover and color for dropdown header title

--- src/components/dropdown/Dropdown.styles.js
+++ src/components/dropdown/Dropdown.styles.js
@@ -7,16 +7,22 @@ export  const Wrapper = styled.div`
 
 export  const Header = styled.div`
     cursor: pointer;
+    color: ${THEME.COLOR.SECONDARY};
+    font-size: ${THEME.FONT.SIZE.MEDIUM}px;
 
     -webkit-touch-callout: none; /* iOS Safari */
     -webkit-user-select: none; /* Safari */
     -moz-user-select: none; /* Old versions of Firefox */
     -ms-user-select: none; /* Internet Explorer/Edge */
+
+    &:hover {
+       color: ${THEME.COLOR.PRIMARY};
+    }
 `;
 
 export  const SelectWrapper = styled.div`
     position: absolute;
-    top: 24px;
+    top: 32px;
     right: 0;
 `;
 
@@ -27,8 +33,9 @@ export  const Select = styled.div`
 
 export  const Option = styled.div`
     cursor: pointer;
-    padding: 2px 4px;
+    padding: 4px 8px;
     color: ${THEME.COLOR.INVERT_3};
+    font-size: ${THEME.FONT.SIZE.MEDIUM}px;
     background: ${THEME.COLOR.SECONDARY_HOVER};
 
     &:hover {
